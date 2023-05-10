b3d20ad38ddb588da0886a8a710ca1c937449c5e
Aleksandra Kasprolewicz <aleksandra.kasprolewicz@gmail.com>
2023-05-10 03:14:53 +0200

    Diagram

---
+++ build-wds-Desktop_x86_windows_msvc2019_pe_32bit-Debug/ui_diagram.h
@@ -0,0 +1,82 @@
+/********************************************************************************
+** Form generated from reading UI file 'diagram.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIAGRAM_H
+#define UI_DIAGRAM_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDoubleSpinBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include "qcustomplot.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Diagram
+{
+public:
+    QCustomPlot *plot;
+    QPushButton *pushButtonAdd;
+    QPushButton *pushButtonClear;
+    QLabel *labelX;
+    QLabel *labelY;
+    QDoubleSpinBox *bx_x;
+    QDoubleSpinBox *bx_y;
+
+    void setupUi(QDialog *Diagram)
+    {
+        if (Diagram->objectName().isEmpty())
+            Diagram->setObjectName("Diagram");
+        Diagram->resize(509, 392);
+        plot = new QCustomPlot(Diagram);
+        plot->setObjectName("plot");
+        plot->setGeometry(QRect(10, 20, 481, 291));
+        pushButtonAdd = new QPushButton(Diagram);
+        pushButtonAdd->setObjectName("pushButtonAdd");
+        pushButtonAdd->setGeometry(QRect(300, 330, 93, 29));
+        pushButtonClear = new QPushButton(Diagram);
+        pushButtonClear->setObjectName("pushButtonClear");
+        pushButtonClear->setGeometry(QRect(410, 330, 93, 29));
+        labelX = new QLabel(Diagram);
+        labelX->setObjectName("labelX");
+        labelX->setGeometry(QRect(20, 330, 21, 20));
+        labelY = new QLabel(Diagram);
+        labelY->setObjectName("labelY");
+        labelY->setGeometry(QRect(140, 330, 31, 20));
+        bx_x = new QDoubleSpinBox(Diagram);
+        bx_x->setObjectName("bx_x");
+        bx_x->setGeometry(QRect(50, 330, 62, 25));
+        bx_y = new QDoubleSpinBox(Diagram);
+        bx_y->setObjectName("bx_y");
+        bx_y->setGeometry(QRect(180, 330, 62, 25));
+
+        retranslateUi(Diagram);
+
+        QMetaObject::connectSlotsByName(Diagram);
+    } // setupUi
+
+    void retranslateUi(QDialog *Diagram)
+    {
+        Diagram->setWindowTitle(QCoreApplication::translate("Diagram", "Dialog", nullptr));
+        pushButtonAdd->setText(QCoreApplication::translate("Diagram", "Dodaj", nullptr));
+        pushButtonClear->setText(QCoreApplication::translate("Diagram", "Wyczy\305\233\304\207", nullptr));
+        labelX->setText(QCoreApplication::translate("Diagram", " X", nullptr));
+        labelY->setText(QCoreApplication::translate("Diagram", "   Y", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Diagram: public Ui_Diagram {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIAGRAM_H
